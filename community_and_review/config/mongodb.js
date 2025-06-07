15d0d561e59ff417c7ed41c947de3ed2e2cddbf4
Abrham A. <abrhamabebe564@gmail.com>
2025-06-07 15:28:20 +0300

    updating mongo url for connecting to atlas mongo server from local

--- community_and_review/config/mongodb.js
+++ community_and_review/config/mongodb.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
