5683b7b959d53da65dfdf017ee85b9d977a119fc
Abrham A. <abrhamabebe564@gmail.com>
2025-06-08 15:51:52 +0300

    adding route for recommendation

--- book_service/routes/booksRoute.js
+++ book_service/routes/booksRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
+<<<<<<< Updated upstream
 import { getTrending, getBook, search, getRelatedBooks, getNewReleasesByGenre, getPopularByGenre} from "../controllers/bookController.js";
+=======
+import { getTrending, getBook, search, getRelatedBooks } from "../controllers/bookController.js";
+import { getRecommendation } from "../controllers/recommendationController.js";
+>>>>>>> Stashed changes
 
 const bookRouter=express.Router();
 
@@ -7,7 +12,11 @@ bookRouter.post('/trending', getTrending);          //Is it really post?
 bookRouter.get('/getBook/:id', getBook);
 bookRouter.get('/search', search);
 bookRouter.get('/relatedBooks', getRelatedBooks);
+<<<<<<< Updated upstream
 bookRouter.get('/new-releases/:genre', getNewReleasesByGenre);
 bookRouter.get('/popular/:genre', getPopularByGenre);
+=======
+bookRouter.post('/recommend', getRecommendation);
+>>>>>>> Stashed changes
 
 export default bookRouter;
